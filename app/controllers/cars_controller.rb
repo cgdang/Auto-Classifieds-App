1545a83cc59f9f94e3c789ee9bd685e073d5ab65
Clarence Dang <cdang@clarences-mbp.gateway.2wire.net>
2014-05-19 00:22:18 -0700

    cars controller created

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,58 @@
+class CarsController < ApplicationController
+
+  before_action :authenticate_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_user, only: [:new, :create]
+
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.contact_email = @current_user.email
+    if @car.save
+      redirect_to cars_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    if @car.update(car_params)
+      redirect_to cars_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path
+  end
+
+end
+
+private
+
+def car_params
+  params.require(:car).permit(:year, :make, :model_name, :mileage, :ext_color, :int_color, :engine, :transmission, :drivetrain, :fuel, :door_count, :description, :price, :contact_email, :picture)
+end
+
+def set_user
+  @user = current_user
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Autoclassifieds::Application.routes.draw do
+  get "cars/index"
+  get "cars/show"
+  get "cars/new"
+  get "cars/create"
+  get "cars/edit"
+  get "cars/update"
+  get "cars/destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

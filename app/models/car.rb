cf2e62c9064b688ba02dc4c402ea768e0a87c5af
Clarence Dang <cdang@Clarences-MacBook-Pro.local>
2014-05-19 09:25:22 -0700

    pre-heroku deploy

--- app/models/car.rb
+++ app/models/car.rb
@@ -22,6 +22,24 @@ class Car
   field :price, type: String
   field :contact_email, type: String
 
+  validates_length_of :year, minimum: 4, maximum: 4, message: "Year required (ex. 2014) "
+  validates_numericality_of :year, message: "Year required"
+  validates_presence_of :make, message: "Make required"
+  validates_presence_of :car_model, message: "Model required"
+  validates_presence_of :mileage, message: "Mileage required"
+  validates_presence_of :ext_color, message: "Exterior color required"
+  validates_presence_of :int_color, message: "Interior color required"
+  validates_presence_of :engine, message: "Engine required"
+  validates_inclusion_of :transmission, in: [ 'Automatic', 'Manual' ], message: "Transmission required"
+  validates_inclusion_of :drivetrain, in: [ 'Front Wheel Drive', 'Rear Wheel Drive', 'AWD/4WD' ], message: "Drivetrain required"
+  validates_inclusion_of :fuel, in: [ 'Gasoline', 'Diesel', 'Hybrid', 'Electric', 'Alternative' ], message: "Fuel type required"
+  validates_inclusion_of :door_count, in: [ 'Two Door', 'Three Door', 'Four Door', 'Five Door' ], message: "Door count required"
+  validates_presence_of :description, message: "Description required"
+  validates_presence_of :price, message: "Price required"
+  validates_presence_of :contact_email, message: "Contact email required"
+  validates_presence_of :picture, message: "Picture of car required"
+
+
   belongs_to :user
 
 end

972085ed00e23d54676034dd4be5ea3333c85b9b
Clarence Dang <cdang@clarences-mbp.gateway.2wire.net>
2014-05-18 20:28:38 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Autoclassifieds::Application.config.secret_key_base = 'bf263c8b94866851f8c29f212527db70d33d7a9f699a3c5998d9870df07e85e938898963d991ff81b697a779a7f47ec78c661c023e18662f39ae60ad50cdcd20'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Autoclassifieds::Application.config.session_store :cookie_store, key: '_autoclassifieds_session'
